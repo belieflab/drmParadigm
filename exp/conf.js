7d80981294073504b335f5130715ed251a2cb5c3
joshkenney <joshuagkenney@gmail.com>
2022-09-06 12:16:22 -0400

    added workerId Math.random in conf.js

--- exp/conf.js
+++ exp/conf.js
@@ -3,7 +3,7 @@
 //******************************************//
 
 workerId = getParamFromURL("workerId");
-
+// workerId = Math.round(Math.random() * 10000, 2);
 const durationForListWords = 150;
 const durationForTargetWords = 3000;
 const fixationDuration = 1000;
